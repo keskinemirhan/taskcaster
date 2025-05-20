83883f2d95623d0930d64035add607fc05962bf7
keskinemirhan <keskinemirhan@gmail.com>
2025-05-20 23:35:57 +0300

    Add panel skeleton

---
+++ taskcaster-frontend/src/database.js
@@ -0,0 +1,39 @@
+export const dashboard = {
+    projects: [
+        {
+            id: "web_application_project",
+            name: "Web Application Project",
+        },
+        {
+            id: "legal_issues",
+            name: "Legal Issues",
+        },
+        {
+            id: "it",
+            name: "IT",
+        },
+    ],
+
+    assigned_tasks: [
+        {
+            id: "unit_tests",
+            name: "Unit Tests",
+            status: "progress",
+        },
+        {
+            id: "end-to-end tests",
+            name: "End-to-End Tests",
+            status: "idle",
+        },
+        {
+            id: "database_backup",
+            name: "Database Backup",
+            status: "warning",
+        },
+        {
+            id: "validation",
+            name: "Validation",
+            status: "progress",
+        },
+    ],
+};
